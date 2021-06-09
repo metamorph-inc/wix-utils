01733648087a0a19747ce1875d30aaebaadb5dce
Kevin Smyth <kevin.m.smyth@gmail.com>
2021-06-09 09:25:22 -0500

    Note Source project_urls

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ setup(
     license="MIT",
     packages=find_packages(),
     install_requires=[],
-    extras_require = {
-        'download':  ["requests"]
-    }
+    extras_require={
+        'download': ["requests"]
+    },
+    project_urls={
+        'Source': 'https://github.com/metamorph-inc/wix-utils',
+    },
 )
